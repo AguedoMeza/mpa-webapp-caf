1ae935642e385b4e95fb0c2baa28fc862ce39b0c
José Serna <jose.serna@mpagroup.mx>
2026-02-25 10:52:02 -0600

    ajustes dploy backend

--- backend/start_backend.bat
+++ backend/start_backend.bat
@@ -1,4 +1,7 @@
 @echo off
 cd /d "C:\inetpub\wwwroot\mpa-webapp-caf\backend"
 call venv\Scripts\activate.bat
+set PYTHONIOENCODING=utf-8
+set PYTHONUTF8=1
+chcp 65001 >nul
 uvicorn main:app --host 0.0.0.0 --port 8003 --reload
